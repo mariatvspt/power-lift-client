0c5e83bd849874f125853a3e7898655e791327e4
Maria Theresia Tyas <theresia.valerie@gmail.com>
2020-03-29 16:42:23 -0500

    handle 404s

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route component={NotFound} /> { /* Finally, catch all unmatched routes. Needs to be in the last line. */ }
     </Switch>
   );
 }
