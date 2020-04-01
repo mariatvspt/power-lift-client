a35309557a9a3b6c6b4146e140362ad09c1abcab
Maria Theresia Tyas <theresia.valerie@gmail.com>
2020-04-01 01:07:01 -0500

    code on login with AWS Cognito commented until setup

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+// import { Auth } from "aws-amplify"; // uncomment when AWS setup is done
 import "./Login.css";
 
 export default function Login(props) {
@@ -10,8 +11,15 @@ export default function Login(props) {
     return email.length > 0 && password.length > 0;
   }
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+  
+    try {
+      // await Auth.signIn(email, password); // uncomment when AWS setup is done
+      alert("Logged in");
+    } catch (e) {
+      alert(e.message);
+    }
   }
 
   return (
