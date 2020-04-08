1c552f49823242933ec8cad72722a622a5e8118e
Maria Theresia Tyas <theresia.valerie@gmail.com>
2020-04-07 22:10:25 -0500

    create signup form

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,14 @@ import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login"
 import AppliedRoute from "./components/AppliedRoute";
+import Signup from "./containers/Signup";
 
 export default function Routes({ appProps }) {
   return (
     <Switch>
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
       <Route component={NotFound} /> { /* Finally, catch all unmatched routes. Needs to be in the last line. */ }
     </Switch>
   );
