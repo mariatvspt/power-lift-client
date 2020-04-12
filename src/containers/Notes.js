06ce921e51ab68a38d9b62141263d70b1596242e
Maria Theresia Tyas <theresia.valerie@gmail.com>
2020-04-12 16:32:31 -0500

    save changes to a note but without deleting the old one

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -2,6 +2,7 @@ import React, { useRef, useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { API, Storage } from "aws-amplify";
 import { onError } from "../libs/errorLib";
+import { s3Upload } from "../libs/awsLib";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import config from "../config";
@@ -52,6 +53,12 @@ export default function Notes() {
     file.current = event.target.files[0];
   }
   
+  function saveNote(note) {
+    return API.put("notes", `/notes/${id}`, {
+      body: note
+    });
+  }
+  
   async function handleSubmit(event) {
     let attachment;
   
@@ -59,13 +66,29 @@ export default function Notes() {
   
     if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
       alert(
-        `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
-          1000000} MB.`
+        `Please pick a file smaller than ${
+          config.MAX_ATTACHMENT_SIZE / 1000000
+        } MB.`
       );
       return;
     }
   
     setIsLoading(true);
+  
+    try {
+      if (file.current) {
+        attachment = await s3Upload(file.current);
+      }
+  
+      await saveNote({
+        content,
+        attachment: attachment || note.attachment
+      });
+      history.push("/");
+    } catch (e) {
+      onError(e);
+      setIsLoading(false);
+    }
   }
   
   async function handleDelete(event) {
