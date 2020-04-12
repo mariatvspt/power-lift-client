69f218a69f43e1874ad7b2be4b9c74477386be84
Maria Theresia Tyas <theresia.valerie@gmail.com>
2020-04-12 11:08:27 -0500

    call the create api

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import { onError } from "../libs/errorLib";
+import { API } from "aws-amplify";
 import config from "../config";
 import "./NewNote.css";
 
@@ -20,19 +21,33 @@ export default function NewNote() {
     file.current = event.target.files[0];
   }
 
-  async function handleSubmit(event) {
-    event.preventDefault();
+async function handleSubmit(event) {
+  event.preventDefault();
 
-    if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
-      alert(
-        `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
-          1000000} MB.`
-      );
-      return;
-    }
-
-    setIsLoading(true);
+  if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
+    alert(
+      `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
+        1000000} MB.`
+    );
+    return;
   }
+
+  setIsLoading(true);
+
+  try {
+    await createNote({ content });
+    history.push("/");
+  } catch (e) {
+    onError(e);
+    setIsLoading(false);
+  }
+}
+
+function createNote(note) {
+  return API.post("notes", "/notes", {
+    body: note
+  });
+}
 
   return (
     <div className="NewNote">
