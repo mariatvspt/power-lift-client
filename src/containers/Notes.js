fac2aa88c0b1bb22d2d5d05a01ee23f6adba23f2
Maria Theresia Tyas <theresia.valerie@gmail.com>
2020-04-12 16:28:04 -0500

    render the note form with save and delete button

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -2,6 +2,10 @@ import React, { useRef, useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { API, Storage } from "aws-amplify";
 import { onError } from "../libs/errorLib";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
+import config from "../config";
+import "./Notes.css";
 
 export default function Notes() {
   const file = useRef(null);
@@ -9,6 +13,8 @@ export default function Notes() {
   const history = useHistory();
   const [note, setNote] = useState(null);
   const [content, setContent] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   useEffect(() => {
     function loadNote() {
@@ -34,7 +40,98 @@ export default function Notes() {
     onLoad();
   }, [id]);
 
+  function validateForm() {
+    return content.length > 0;
+  }
+  
+  function formatFilename(str) {
+    return str.replace(/^\w+-/, "");
+  }
+  
+  function handleFileChange(event) {
+    file.current = event.target.files[0];
+  }
+  
+  async function handleSubmit(event) {
+    let attachment;
+  
+    event.preventDefault();
+  
+    if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
+      alert(
+        `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
+          1000000} MB.`
+      );
+      return;
+    }
+  
+    setIsLoading(true);
+  }
+  
+  async function handleDelete(event) {
+    event.preventDefault();
+  
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this note?"
+    );
+  
+    if (!confirmed) {
+      return;
+    }
+  
+    setIsDeleting(true);
+  }
+  
   return (
-    <div className="Notes"></div>
+    <div className="Notes">
+      {note && (
+        <form onSubmit={handleSubmit}>
+          <FormGroup controlId="content">
+            <FormControl
+              value={content}
+              componentClass="textarea"
+              onChange={e => setContent(e.target.value)}
+            />
+          </FormGroup>
+          {note.attachment && (
+            <FormGroup>
+              <ControlLabel>Attachment</ControlLabel>
+              <FormControl.Static>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={note.attachmentURL}
+                >
+                  {formatFilename(note.attachment)}
+                </a>
+              </FormControl.Static>
+            </FormGroup>
+          )}
+          <FormGroup controlId="file">
+            {!note.attachment && <ControlLabel>Attachment</ControlLabel>}
+            <FormControl onChange={handleFileChange} type="file" />
+          </FormGroup>
+          <LoaderButton
+            block
+            type="submit"
+            bsSize="large"
+            bsStyle="primary"
+            isLoading={isLoading}
+            disabled={!validateForm()}
+          >
+            Save
+          </LoaderButton>
+          <LoaderButton
+            block
+            bsSize="large"
+            bsStyle="danger"
+            onClick={handleDelete}
+            isLoading={isDeleting}
+          >
+            Delete
+          </LoaderButton>
+        </form>
+      )}
+    </div>
   );
 }
